89e7a9a7a454a40faefbd30241fc06101b3bf55e
TechLine <cocacola@emile.com>
2021-11-22 20:36:25 +0600

    Initial commit

---
+++ src/com/company/Bank.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Bank {
+    private String passvord;
+    private int amount;
+
+    public String getPassvord() {
+        return passvord;
+    }
+
+    public void setPassvord(String passvord) {
+        this.passvord = passvord;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Bank bank = new Bank();
+        bank.setPassvord("hasanboy");
+        bank.setAmount(1000);
+    }
+}
